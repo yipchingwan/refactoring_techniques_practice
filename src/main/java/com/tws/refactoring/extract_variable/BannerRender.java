aa9e5569174ca2a7f37a421f2692424ed8621e0e
Zhen She <neal.seah@gmail.com>
2018-11-26 03:21:33 +0800

    feat: add quiz for extract-variable

---
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -0,0 +1,10 @@
+package com.tws.refactoring.extract_variable;
+
+public class BannerRender {
+    void renderBanner(String platform, String browser) {
+        if ((platform.toUpperCase().indexOf("MAC") > -1) &&
+                (browser.toUpperCase().indexOf("IE") > -1)) {
+            // do something
+        }
+    }
+}
